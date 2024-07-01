42c03ccc423f15d39ae8e8f9dae6cbaa785e70a2
EvaAvocado <evaavocadic@gmail.com>
2024-07-01 18:24:51 +0300

    Edit Ropes animations

--- Assets/Scripts/Items/RopeAnimation.cs
+++ Assets/Scripts/Items/RopeAnimation.cs
@@ -8,9 +8,19 @@ namespace Items
         [SerializeField] private RopeItem _ropeLeft;
         [SerializeField] private RopeItem _ropeRight;
         [SerializeField] private Animator _animator;
+        [SerializeField] private SpriteRenderer _sprite;
+
+        private SpriteRenderer _ropeLeftSprite;
+        private SpriteRenderer _ropeRightSprite;
         
         private static readonly int Left = Animator.StringToHash("left");
         private static readonly int Right = Animator.StringToHash("right");
+
+        private void Awake()
+        {
+            _ropeLeftSprite = _ropeLeft.GetComponent<SpriteRenderer>();
+            _ropeRightSprite = _ropeRight.GetComponent<SpriteRenderer>();
+        }
 
         private void OnEnable()
         {
@@ -36,11 +46,13 @@ namespace Items
 
         public void ChangeLeftState()
         {
+            _sprite.sortingOrder = _ropeLeftSprite.sortingOrder;
             _animator.SetTrigger(Left);
         }
 
         public void ChangeRightState()
         {
+            _sprite.sortingOrder = _ropeRightSprite.sortingOrder;
             _animator.SetTrigger(Right);
         }
 

09c78a3e129743e95f7233e1fbda23718b5c479b
EvaAvocado <evaavocadic@gmail.com>
2024-07-04 19:07:38 +0300

    Add Outro Exit

---
+++ Assets/Scripts/Outro/OutroManager.cs
@@ -0,0 +1,18 @@
+using Intro;
+using UnityEngine;
+
+public class OutroManager : MonoBehaviour
+{
+    [SerializeField] private Fade _fade;
+    [SerializeField] private Animator _animator;
+
+    public void Load10Level()
+    {
+        _fade.FadeIn();
+    }
+
+    public void PlayAnimation()
+    {
+        _animator.Play("cutscene");
+    }
+}

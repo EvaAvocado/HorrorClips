c456fbb244c1e6925e2e56f2e5ca491acecdab9e
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2024-07-04 21:42:30 +0300

    fix wall

--- Assets/Scripts/Level/Clips/Deadend.cs
+++ Assets/Scripts/Level/Clips/Deadend.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Utils;
 
@@ -15,7 +16,15 @@ namespace Level.Clips
                  _collider.isTrigger = true;
              }
          }
-        
+
+         private void OnCollisionStay2D(Collision2D other)
+         {
+             if (_stopWall.ClipLayer.Contains(other.gameObject.layer) && !_stopWall.IsDeadEnd)
+             {
+                 _collider.isTrigger = true;
+             }
+         }
+
          private void OnTriggerExit2D(Collider2D other)
          {
              if (_stopWall.ClipLayer.Contains(other.gameObject.layer) && !_stopWall.IsDeadEnd)
--- Assets/Scripts/Level/Clips/StopWall.cs
+++ Assets/Scripts/Level/Clips/StopWall.cs
@@ -11,28 +11,11 @@ namespace Level
         [SerializeField] private bool _isDeadEnd;
 
         public bool IsDeadEnd => _isDeadEnd;
-
         public LayerMask ClipLayer => _clipLayer;
 
-        // private void OnTriggerEnter2D(Collider2D other)
-        // {
-        //     if (_clipLayer.Contains(other.gameObject.layer) && !_isDeadEnd)
-        //     {
-        //         _collider.isTrigger = true;
-        //     }
-        // }
-        //
-        // private void OnTriggerExit2D(Collider2D other)
-        // {
-        //     if (_clipLayer.Contains(other.gameObject.layer) && !_isDeadEnd)
-        //     {
-        //         _collider.isTrigger = false;
-        //     }
-        // }
-
-        public void EnableWall(bool isEnable){
+        public void EnableWall(bool isEnable)
+        {
             _collider.isTrigger = isEnable;
-            //Debug.Log(name);
         }
 
         public bool GetEnableWall => _collider.isTrigger;

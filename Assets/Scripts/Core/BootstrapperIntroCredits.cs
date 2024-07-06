8e88cb25a104c9a75f673e185c768d80df47be4c
EvaAvocado <evaavocadic@gmail.com>
2024-07-06 12:10:20 +0300

    Fix bug with audio

--- Assets/Scripts/Core/BootstrapperIntroCredits.cs
+++ Assets/Scripts/Core/BootstrapperIntroCredits.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace Core
 {
     public class BootstrapperIntroCredits : MonoBehaviour
     {
         [SerializeField] private MenuManager _menuManager;
+        [SerializeField] private AudioMixer _audioMixer;
+        [SerializeField] private string _pitchParameterMusic;
 
         private void Start()
         {
+            SetPitch(_pitchParameterMusic);
             _menuManager.Init();
+        }
+        
+        private void SetPitch(string parameter)
+        {
+            _audioMixer.SetFloat(parameter, 1);
         }
     }
 }

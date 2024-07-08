f4bcf99eacbc12a5134d4420a2a9a05bb8b13e23
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2024-07-08 18:13:30 +0300

    add menu for level selection

---
+++ Assets/Scripts/UI/LevelSelection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class LevelSelection : MonoBehaviour
+    {
+        [SerializeField] private List<Button> _buttonForSelection;
+
+        private void Start()
+        {
+            for (int i = 0; i < _buttonForSelection.Count; i++)
+            {
+                if (PlayerPrefs.GetInt($"{i + 1}") == 1)
+                {
+                    _buttonForSelection[i].interactable = true;
+                    var i1 = i + 1;
+                    _buttonForSelection[i].onClick.AddListener(() => SceneManager.LoadScene(i1));
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
